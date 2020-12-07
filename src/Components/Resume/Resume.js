f4980efb13c1d65c290956774c97b62907ebadf3
Matthew Leiman <matthewtleiman@gmail.com>
2020-12-07 10:37:21 -0600

    added updated resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,19 +1,15 @@
 import React from "react";
-import Pdf from "./files/Resume.js.pdf";
-import {Button} from "react-bootstrap";
+import Pdf from "./files/mlResume2020.pdf";
+import { Button } from "react-bootstrap";
 export default function Resume() {
   return (
-     <div>
-        <div className="content justify-content-center row my-4">
-          <a
-            href={Pdf}
-            target="_blank"
-          >
-            {" "}
-            <Button variant="dark">Check Out My Resume</Button>
-          </a>
-        </div>
-        </div>
-
+    <div>
+      <div className="content justify-content-center row my-4">
+        <a href={Pdf} target="_blank">
+          {" "}
+          <Button variant="dark">Check Out My Resume</Button>
+        </a>
+      </div>
+    </div>
   );
 }
