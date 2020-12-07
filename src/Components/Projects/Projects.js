8b85f28ce46f1897b5c13a935576ad3b8436552c
Matthew Leiman <matthewtleiman@gmail.com>
2020-12-07 10:59:52 -0600

    update

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,7 +8,27 @@ const Projects = () => {
           <h3>Created Projects</h3>
         </div>
         <div className="col-12 col-lg-5 App rounded my-2 mx-2 p-2">
-          <a href="https://github.com/mleiman0822/Solo-Project-Rinx">
+          <a>
+            <div className="btn col-12">North Dakota Dental Foundation</div>
+          </a>
+          Created a full stack inventory management system for NDDF in Fargo,
+          North Dakota. The North Dakota Dental Foundation is a non-profit
+          organization that provides dental equipment to dentists with the
+          mission to provide oral health to those who are unable to get it.
+          <br />
+          <br />
+          <div>
+            <Button
+              className="gitHubButton"
+              href="https://github.com/North-Dakota-Dental-Foundation/dental-rental"
+            >
+              {" "}
+              GitHub
+            </Button>
+          </div>
+        </div>
+        <div className="col-12 col-lg-5 App rounded my-2 mx-2 p-2">
+          <a>
             <div className="btn col-12">RINX</div>
           </a>
           Final Solo Project for EDA. This is something that im proud of as it
@@ -17,10 +37,27 @@ const Projects = () => {
           the ability to add new rinks(with admin access), add rinks to your
           favorites, and check the status of a rink. This app uses A WeatherBox
           and a MapBox API.
+          <div>
+            <Button
+              className="gitHubButton"
+              href="https://github.com/mleiman0822/Solo-Project-Rinx"
+            >
+              {" "}
+              GitHub
+            </Button>
+            {(" ", " ", " ")}
+            <Button
+              className="gitHubButton"
+              href="https://calm-scrubland-45507.herokuapp.com/#/home"
+            >
+              {" "}
+              Heroku
+            </Button>
+          </div>
         </div>
         <div className="col-12 col-lg-5 App rounded my-2 mx-2 p-2">
           <div>
-            <a href="https://github.com/mleiman0822/movie-sagas">
+            <a>
               <div className="btn col-12">Movie Gallery</div>
             </a>
           </div>
@@ -28,30 +65,48 @@ const Projects = () => {
             This application allows users to select from a list of movies and
             gives them the ability to edit some aspects and details of each
             movie.
+            <br />
+            <br />
+            <div>
+              <Button
+                className="gitHubButton"
+                href="https://github.com/mleiman0822/movie-sagas"
+              >
+                {" "}
+                GitHub
+              </Button>
+            </div>
           </div>
         </div>
         <div className="col-12 col-xl-5 App rounded my-2 mx-2 p-2">
-          <a href="https://github.com/mleiman0822/redux-dashboard">
-            <div className="btn col-12">Redux Dashboard</div>
-          </a>
-          A Redux Application that allows users to change speed using a
-          controller. This is accomplished using reducers to transfer props
-          between components.
-        </div>
-        <div className="col-12 col-xl-5 App rounded my-2 mx-2 p-2">
           <div>
-            <a href="https://github.com/mleiman0822/react-gallery">
+            <a>
               <div className="btn col-12">React-Gallery</div>
             </a>
           </div>
           Social Media type application that was made with React. Contains the
           ability to like photos. Uses Bootstrap.
+          <div>
+            <br />
+            <br />
+            <Button
+              className="gitHubButton"
+              href="https://github.com/mleiman0822/react-gallery"
+            >
+              {" "}
+              GitHub
+            </Button>
+          </div>
         </div>{" "}
       </div>
       <div>
+        <br />
         <Button variant="dark" href="https://github.com/mleiman0822">
           Check out some more of my projects on GitHub
         </Button>
+        <br />
+        <br />
+        <br />
       </div>
     </div>
   );
